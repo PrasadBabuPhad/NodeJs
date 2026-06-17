21360f0aeb8389519f732c6de95703374638c8fb
PrasadBabuPhad <pphad4925@gmail.com>
2026-06-17 16:48:48 +0530

    authentication in each block

---
+++ Day9/index.js
@@ -0,0 +1,95 @@
+const express=require('express');
+const app=express();
+
+app.use(express.json());
+const FoodMenu=[
+    {id:1,food:"Noodles",category:"veg",price:150},
+    {id:2,food:"Butter Naan",category:"veg",price:50},
+    {id:3,food:"Rajma",category:"veg",price:200},
+    {id:4,food:"bobby",category:"veg",price:10},
+    {id:5,food:"Momo",category:"veg",price:100},
+    {id:6,food:"Green Peas",category:"veg",price:250},
+    {id:7,food:"Kaju Panner",category:"veg",price:400}
+];
+const AddToCart=[];
+
+app.get("/food",(req,res)=>{
+    res.status(200).send(FoodMenu);
+})
+
+app.post("/admin",(req,res)=>{
+    //Add the item into food menu
+    //Authentication karna padega ki kya ye admin hai
+
+    const token="ABCDEF"
+    const Access=token==="ABCDEF"?1:0;
+
+    if(Access){
+        FoodMenu.push(req.body);
+        res.status(201).send("Items Can be added");
+    }
+    else{
+        res.status(202).send("Items Cant be added");
+    }
+
+})
+
+app.delete("/admin/:id",(req,res)=>{
+    //Add the item into food menu
+    //Authentication karna padega ki kya ye admin hai
+
+    const token="ABCDEF"
+    const Access=token==="ABCDEF"?1:0;
+
+    if(Access){
+        const id=parseInt(req.params.id);
+        const index=FoodMenu.findIndex(item=> item.id===id);
+        if(index===-1){
+            res.send("Item Doesn't Exists");
+        }
+        else{
+            FoodMenu.splice(index,1);
+            res.send("Succesfully Deleted");
+        }
+    }
+    else{
+        res.status(403).send("No Permission");
+    }
+})
+
+app.patch("/admin",(req,res)=>{
+    //Add the item into food menu
+    //Authentication karna padega ki kya ye admin hai
+
+    const token="ABCDEF"
+    const Access=token==="ABCDEF"?1:0;
+
+    if(Access){
+        const id=req.body.id;
+
+        const fooddata=FoodMenu.find(item=> item.id===id)
+
+        if(fooddata){
+            if(req.body.food)
+                fooddata.food=req.body.food;
+            if(req.body.category)
+                fooddata.category=req.body.category;
+            if(req.body.price)
+                fooddata.price=req.body.price;
+
+            res.send("Successfully Updated");
+        }
+        else{
+            res.send("Item not exist")
+        }
+    }
+    else{
+        res.status(403).send("No Permission")
+    }
+})
+
+//localhost:3000/admin
+
+app.listen(3000,()=>{
+    console.log("Listening at port 3000");
+})
