1a8fcfeb3ee422a55dab428b2c931758e76948bb
PrasadBabuPhad <pphad4925@gmail.com>
2026-06-20 21:33:59 +0530

    mongodb connection setup

---
+++ Day13Mongodb/database.js
@@ -0,0 +1,12 @@
+const {MongoClient}= require('mongodb');
+
+
+const url="mongodb+srv://lucy:Prasad@123@codingadda.xifhqtg.mongodb.net/";
+
+const client= new MongoClient(url);
+
+const dbName= 'CoderArmy'
+
+
+
+
