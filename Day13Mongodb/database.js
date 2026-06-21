1c4ea2d72b991a705ea6b0119e75b3babd28fd34
PrasadBabuPhad <pphad4925@gmail.com>
2026-06-21 15:29:38 +0530

    mongodb connection with server and info display

--- Day13Mongodb/database.js
+++ Day13Mongodb/database.js
@@ -1,11 +1,34 @@
 const {MongoClient}= require('mongodb');
 
-
-const url="mongodb+srv://lucy:Prasad@123@codingadda.xifhqtg.mongodb.net/";
+// @ == %40
+// @ == hexadecimal: 0x40
+const url="mongodb+srv://lucy:Prasad%40123@codingadda.xifhqtg.mongodb.net/";
 
 const client= new MongoClient(url);
 
-const dbName= 'CoderArmy'
+const dbName= 'CoderArmy'; //Database name
+
+async function main(){
+    //use connect method to connect
+
+    await client.connect();
+    console.log('Connected successfully to server');
+    const db=client.db(dbName);
+    const collection=db.collection('user');
+
+    const findResult=collection.find({})
+    //const ans= await findResult.toArray();
+    console.log('Found documents =>',ans);
+
+
+
+    return 'done.';
+}
+
+main()
+    .then(console.log)
+    .catch(console.error)
+    .finally(()=>client.close());
 
 
 
