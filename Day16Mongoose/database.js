039b73993371ab877470ee0494b815a518933b30
PrasadBabuPhad <pphad4925@gmail.com>
2026-06-24 22:29:04 +0530

    find

--- Day16Mongoose/database.js
+++ Day16Mongoose/database.js
@@ -1,27 +1,26 @@
-const mongoose =require('mongoose');
-
+import mongoose from 'mongoose';
+const {Schema} =mongoose;
 async function main(){
-    await mongoose.connect("mongodb+srv://lucy:Prasad%40123@codingadda.xifhqtg.mongodb.net/BookStore");
+    await mongoose.connect("mongodb+srv://lucy:Prasad%40123@codingadda.xifhqtg.mongodb.net/Bookstore");
+                            //mongodb+srv://lucy:<db_password>@codingadda.xifhqtg.mongodb.net/
+
+    const userSchema= new Schema({
+        name:String,
+        age:Number,
+        city:String,
+        gender:String
+    })
+
+    const User=mongoose.model("user",userSchema);
 
 
-const userSchema= new Schema({
-    name:String,
-    age:Number,
-    city:String,
-    gender:String
-})
+    const user1= new User({name:"Rohit",age:20,city:"dwarka",gender:"Male"});
+    await user1.save();
 
-const User=mongoose.model("user",userSchema);
+    await User.create({name:"Mohan",city:"Jaipur",age:30});
 
-
-const user1= new User({name:"Rohit",age:20,city:"dwarka",gender:"Male"});
-await user1.save();
-
-await User.create({name:"Mohan",city:"Jaipur",age:30});
-
-
-
-
+    const ans=await User.find({});
+    console.log(ans);
 }
 main()
 .then(()=>console.log("Connected to database"))
