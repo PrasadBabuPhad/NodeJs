779ee76c67b4c10dd2fc78ab206e1904db64f4e5
PrasadBabuPhad <pphad4925@gmail.com>
2026-06-25 16:59:12 +0530

    getting get data

--- Day16Mongoose/database.js
+++ Day16Mongoose/database.js
@@ -1,27 +1,28 @@
-import mongoose from 'mongoose';
+const mongoose=require("mongoose");
 const {Schema} =mongoose;
+
+
 async function main(){
     await mongoose.connect("mongodb+srv://lucy:Prasad%40123@codingadda.xifhqtg.mongodb.net/Bookstore");
                             //mongodb+srv://lucy:<db_password>@codingadda.xifhqtg.mongodb.net/
 
-    const userSchema= new Schema({
-        name:String,
-        age:Number,
-        city:String,
-        gender:String
-    })
+    // const userSchema= new Schema({
+    //     name:String,
+    //     age:Number,
+    //     city:String,
+    //     gender:String
+    // })
 
-    const User=mongoose.model("user",userSchema);
+    // const User=mongoose.model("user",userSchema);
 
 
-    const user1= new User({name:"Rohit",age:20,city:"dwarka",gender:"Male"});
-    await user1.save();
+    // const user1= new User({name:"Rohit",age:20,city:"dwarka",gender:"Male"});
+    // await user1.save();
 
-    await User.create({name:"Mohan",city:"Jaipur",age:30});
+    // await User.create({name:"Mohan",city:"Jaipur",age:30});
 
-    const ans=await User.find({});
-    console.log(ans);
+    // const ans=await User.find({});
+    // console.log(ans);
 }
-main()
-.then(()=>console.log("Connected to database"))
-.catch((err)=>console.log(err));
+
+module.exports=main;
--- Day16Mongoose/index.js
+++ Day16Mongoose/index.js
@@ -1,10 +1,30 @@
-import express from 'express';
+const express =require("express");
 const app=express();
-import './database.js';
+const main=require("./database.js");
+const user=require("./users.js");
+
 
 app.use(express.json());
 
 
-app.listen(3000,()=>{
+app.get("/info", async (req,res)=>{
+
+    const ans=await user.find({});
+    res.send(ans);
+})
+
+
+//app.post()
+
+
+main()
+.then(async ()=>{
+    console.log("Connected to database");
+    app.listen(3000,()=>{
     console.log("Listening at port 3000");
-});
+    })
+
+    // const result=await user.find({name:"Rohit"});
+    // console.log(result);
+})
+.catch((err)=>console.log(err));
